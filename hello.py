80c0b3931948f72ff61b2bac547c31454d0bca5d
Kevin Bowen <kevin.bowen@gmail.com>
2022-01-19 11:39:38 -0800

    add user agent response

---
+++ hello.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
+    # return '<h1>Hello World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
