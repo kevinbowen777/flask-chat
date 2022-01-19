4e5a2de3b6a565796b17765337076f044eb69e40
Kevin Bowen <kevin.bowen@gmail.com>
2022-01-19 15:47:54 -0800

    add render_template

--- hello.py
+++ hello.py
@@ -1,16 +1,14 @@
 from flask import Flask
-from flask import request
+from flask import render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is {}</p>'.format(user_agent)
-    # return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
 
