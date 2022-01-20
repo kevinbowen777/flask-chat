8364d14e2600f3be1e9d94a27b7e4d74c3028015
Kevin Bowen <kevin.bowen@gmail.com>
2022-01-20 00:21:08 -0800

    add name submission handling

--- hello.py
+++ hello.py
@@ -1,25 +1,20 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'this is not the real secret key'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -31,4 +26,13 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
