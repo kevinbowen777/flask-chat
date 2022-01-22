90aad9aac869b5faf1a54f7b99eb148617ac2a8d
Kevin Bowen <kevin.bowen@gmail.com>
2022-01-22 14:01:24 -0800

    Create package constructor blueprint

--- hello.py
+++ hello.py
@@ -1,25 +1,12 @@
 import os
 from flask import Flask, render_template, session, redirect, url_for
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_mail import Mail
 from flask_mail import Message
-from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-mail = Mail(app)
-
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
