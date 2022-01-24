b4f6ad559b8ec5995c46f9534422030f19cac730
Kevin Bowen <kevin.bowen@gmail.com>
2022-01-23 16:17:25 -0800

    Add view function to confirm user accounts

--- app/auth/views.py
+++ app/auth/views.py
@@ -44,3 +44,16 @@ def register():
         flash('A confirmation email has been sent to you by email.')
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        db.session.commit()
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
