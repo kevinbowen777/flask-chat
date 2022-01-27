dbe5daadc8b3b16a50e59ae045146de902ca8521
Kevin Bowen <kevin.bowen@gmail.com>
2022-01-27 15:19:40 -0800

    API blueprint creation

--- app/main/views.py
+++ app/main/views.py
@@ -224,7 +224,7 @@ def show_followed():
 def moderate():
     page = request.args.get('page', 1, type=int)
     pagination = Comment.query.order_by(Comment.timestamp.desc()).paginate(
-        page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+        page, per_page=current_app.config['FLASK_COMMENTS_PER_PAGE'],
         error_out=False)
     comments = pagination.items
     return render_template('moderate.html', comments=comments,
