926b190c1d754317919359a5c3de20813188a2e1
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-03 13:19:43 -0800

    Add handling for uri prefix

--- config.py
+++ config.py
@@ -40,10 +40,9 @@ class TestingConfig(Config):
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
     @classmethod
     def init_app(cls, app):
