a094bf3391847151fb1f6b15bc319214596e2d7e
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-03 14:36:26 -0800

    rollback migrate()

--- app/__init__.py
+++ app/__init__.py
@@ -6,14 +6,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_pagedown import PageDown
 from config import config
-from flask_migrate import Migrate
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 pagedown = PageDown()
-migrate = Migrate()
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
@@ -30,7 +28,6 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     pagedown.init_app(app)
-    migrate.init_app(app, db)
 
     if app.config['SSL_REDIRECT']:
         from flask_sslify import SSLify
