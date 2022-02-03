d50f96bc979b4609122de654ae7122c37aced840
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-03 11:53:03 -0800

    Add db URI fix for postgres

--- config.py
+++ config.py
@@ -42,6 +42,8 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
     @classmethod
     def init_app(cls, app):
