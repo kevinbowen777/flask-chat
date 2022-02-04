e5528b812fbe11005d50f5319b6ba9cb566c5ab7
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-04 10:35:06 -0800

    Add container startup script

--- config.py
+++ config.py
@@ -100,7 +100,6 @@ class DockerConfig(ProductionConfig):
         import logging
         from logging import StreamHandler
         file_handler = StreamHandler()
-        file_handler = StreamHandler()
         file_handler.setLevel(logging.INFO)
         app.logger.addHandler(file_handler)
 
