ce50f1fccc54d3672beb705b3788b160b3f612e2
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-04 14:51:21 -0800

    Set wait & retry for db to start

--- boot.sh
+++ boot.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 source venv/bin/activate
-flask deploy
+
+while true: do
+    flask deploy
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Flask Deploy command failed, retrying in 5 seconds...
+    sleep 5
+done
+
 exec gunicorn -b :5000 --access-logfile - --error-logfile - flask_chat:app
